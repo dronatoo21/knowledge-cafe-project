560a91e56d66026ed7af5340b7a4369ce7df9c6c
dronatoo <mrdronatoo@gmail.com>
2023-09-14 11:47:50 -0700

    first commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+    return (
+        <div>
+           <h1 className="text-4xl font-semibold">Knowledge Cafe</h1> 
+           <img src="https://i.ibb.co/L1b6xSq/shape.jpg" alt="img" />
+        </div>
+    );
+};
+
+export default Header;
